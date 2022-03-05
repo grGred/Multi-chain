db3e46a61c840519334a8a9f9c7169064ab23ef2
Vladislav <81467635+grGred@users.noreply.github.com>
2022-03-05 16:24:10 +0300

    add swap logic

---
+++ contracts/interfaces/IPeggedToken.sol
@@ -0,0 +1,9 @@
+
+
+pragma solidity >=0.8.9;
+
+interface IPeggedToken {
+    function mint(address _to, uint256 _amount) external;
+
+    function burn(address _from, uint256 _amount) external;
+}
---
+++ contracts/interfaces/IWETH.sol
@@ -0,0 +1,9 @@
+
+
+pragma solidity >=0.8.9;
+
+interface IWETH {
+    function deposit() external payable;
+
+    function withdraw(uint256) external;
+}
